e042e0d5fcc87d887ddc95c2f10fefba9d74954e
AndrewMbugua <andrewmbugua388@gmail.com>
2022-09-20 11:20:10 +0300

    code improvements

--- tcp-client-server/server.c
+++ tcp-client-server/server.c
@@ -2,11 +2,11 @@
 
 /*
 notes
-kill server process - sudo kill $(sudo lsof -t -i:53474)
+kill server process - sudo kill $(sudo lsof -t -i:port_set)
 valgrind test - valgrind --tool=memcheck --leak-check=full ./tcp_server
 */
 
-#define PORT "5374" //port users will be connected to
+#define PORT "53574" //port users will be connected to
 
 #define BACKLOG 10 //how many pending connections queue will hold
 
@@ -24,7 +24,7 @@ int main(){
 /* listen on sock_fd
    receive new connection on few_fd
 */
-int sockfd, new_fd;
+int sockfd, new_connection;
 
 struct addrinfo hints, *server_info, *p;
 struct sockaddr_storage client_addr;
@@ -33,19 +33,23 @@ socklen_t sin_size;
 //struct signal_action sa; //look into later
 int yes = 1; //to modify later
 
-//INET6_ADDRSTRLEN - for IPv4 mapped addreses,has 46 bytes
-char mapped_addresses[INET6_ADDRSTRLEN]; 
-int receive;
-
-//hints - a pointer to a struct addrinfo
+/*INET6_ADDRSTRLEN - for IPv4 mapped addreses,has 46 bytes
+getaddrinfo - eliminates IPv4 & IPv6 dependencies
+hints - a pointer to a struct addrinfo
+*/
 memset(&hints, 0 ,sizeof hints);
 hints.ai_socktype = SOCK_STREAM;
 hints.ai_flags = AI_PASSIVE; //use 127.0.0.1
 
-/* error checkers*/
-if ((receive = getaddrinfo(NULL, PORT, &hints, &server_info)) != 0)
+char mapped_addresses[INET6_ADDRSTRLEN]; 
+int get_address_info = getaddrinfo(NULL, PORT, &hints, &server_info);
+
+
+// error checkers
+if (get_address_info != 0)
 {
-fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(receive));
+//gai_strerror - translates the error codes to a human readable string
+fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(get_address_info));
 return 1;
 }
 
@@ -54,7 +58,7 @@ for (server_info; server_info != NULL; server_info -> ai_next)
 {
     if((sockfd = socket(server_info -> ai_family, server_info -> ai_socktype, server_info -> ai_protocol )) == -1)
     {
-        perror("server:socket");
+        perror("server socket error");
         continue;
     }
 
@@ -76,6 +80,7 @@ break;
 
 freeaddrinfo(server_info);
 
+
 if(server_info == NULL)
 {
     fprintf(stderr, "server failed to bind\n");
@@ -89,15 +94,14 @@ if(listen(sockfd, BACKLOG) == -1)
 }
 
 
-
 printf("Andrew's server, waiting for connections.\n");
 
 while(1)
 {
     sin_size = sizeof client_addr;
     //await a connection to fd
-    new_fd = accept(sockfd,(struct sockaddr*) &client_addr, &sin_size);
-    if(new_fd == -1)
+    new_connection = accept(sockfd,(struct sockaddr*) &client_addr, &sin_size);
+    if(new_connection == -1)
     {
     perror("accept");
     continue;
@@ -106,8 +110,7 @@ while(1)
 inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr*)&client_addr),mapped_addresses, sizeof mapped_addresses);
 printf("Andrew's server: got connection from %s\n", mapped_addresses);
 }
-close(new_fd);
-
+close(new_connection);
 
 
 return 0;
